e9bca762898f655096b4450ffb4a4ce3da71d748
Mako <Mako.wusstig@outlook.com>
2026-06-24 20:49:02 -0700

    updated html and added tailwind css

---
+++ login.js
@@ -0,0 +1,8 @@
+// =========================================
+// LOGIN LOGIC (login.js)
+// =========================================
+
+// ========== Zach Section ==========
+// TODO: Validate login form
+// TODO: Fake authentication
+// TODO: Redirect to landing.html
